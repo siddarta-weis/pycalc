46f47ad51d129a52e0df0fd606fe6f9258056f1e
siddarta-weis <siddarta.weis@gmail.com>
2021-09-20 07:46:57 -0300

    Refactored calc_percentage()

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Calculadora(QWidget):
 
         self.initialize_ui()
 
-    def initialize_ui(self):
+    def initialize_ui(self) -> None:
         self.setFixedSize(300, 400)
         self.setWindowTitle("Pycalc")
         self.setWindowFlags(Qt.FramelessWindowHint)
@@ -66,7 +66,7 @@ class Calculadora(QWidget):
 
         self.add_button(QPushButton("C"), 2, 0, 1, 1, lambda: self.line_edit.setText(""))
         self.add_button(QPushButton("<"), 2, 1,1,1,lambda: self.line_edit.setText(self.line_edit.text()[:-1]),)
-        self.add_button(QPushButton("%"), 2, 2, 1, 1, self.percentage)
+        self.add_button(QPushButton("%"), 2, 2, 1, 1, self.calc_percentage)
         self.add_button(QPushButton("÷"), 2, 3, 1, 1, style=True)
 
         self.add_button(QPushButton("7"), 3, 0, 1, 1)
@@ -105,7 +105,7 @@ class Calculadora(QWidget):
             button.setStyleSheet("* {background: #f5923d; color: white; border:1px solid #cf884a;}")
 
 
-    def calculate(self):
+    def calculate(self) -> None:
         to_eval = self.line_edit.text().replace("x", "*")
         to_eval = to_eval.replace("÷", "/")
         try:
@@ -113,53 +113,66 @@ class Calculadora(QWidget):
         except Exception as e:
             self.line_edit.setText("Value Error")
 
-    def percentage(self):
+    def calc_percentage(self) -> None:
         if "÷" in self.line_edit.text():
-            try:
-                num, percent = self.line_edit.text().split("÷")
-                num, percent = float(num), float(percent)
-                result = num / (percent/100)
-                self.line_edit.setText(str(self.safe_to_int(result)))
-            except Exception as e:
-                self.line_edit.setText("Value Error")
+            self.line_edit.setText(self.calc_percent_div())
 
         if "x" in self.line_edit.text():
-            try:
-                num, percent = self.line_edit.text().split("x")
-                num, percent = float(num), float(percent)
-                result = (num / 100) * percent
-                self.line_edit.setText(str(self.safe_to_int(result)))
-            except Exception as e:
-                self.line_edit.setText("Value Error")
+            self.line_edit.setText(self.calc_percent_mult())
+
         if "-" in self.line_edit.text():
-            try:
-                num, percent = self.line_edit.text().split("-")
-                num, percent = float(num), float(percent)
-                result = num - (num / 100) * percent
-                self.line_edit.setText(str(self.safe_to_int(result)))
-            except Exception as e:
-                self.line_edit.setText("Value Error")
+            self.line_edit.setText(self.calc_percent_sub())
 
         if "+" in self.line_edit.text():
-            try:
-                num, percent = self.line_edit.text().split("+")
-                num, percent = float(num), float(percent)
-                result = num + (num / 100) * percent
-                self.line_edit.setText(str(self.safe_to_int(result)))
-            except Exception as e:
-                print(e)
-                self.line_edit.setText("Value Error")
+            self.line_edit.setText(self.calc_percent_sum())
     
+    def calc_percent_div(self) -> str:
+        try:
+            num, percent = self.line_edit.text().split("÷")
+            num, percent = float(num), float(percent)
+            result = num / (percent/100)
+            return str(self.safe_to_int(result))
+        except Exception as e:
+            return "Value Error"
+    
+    def calc_percent_mult(self) -> str:
+        try:
+            num, percent = self.line_edit.text().split("x")
+            num, percent = float(num), float(percent)
+            result = (num / 100) * percent
+            return str(self.safe_to_int(result))
+        except Exception as e:
+            return "Value Error"
+
+    def calc_percent_sub(self) -> str:
+        try:
+            num, percent = self.line_edit.text().split("-")
+            num, percent = float(num), float(percent)
+            result = num - (num / 100) * percent
+            return str(self.safe_to_int(result))
+        except Exception as e:
+            return "Value Error"
+
+    def calc_percent_sum(self) -> str:
+        try:
+            num, percent = self.line_edit.text().split("+")
+            num, percent = float(num), float(percent)
+            result = num + (num / 100) * percent
+            return str(self.safe_to_int(result))
+        except Exception as e:
+            return "Value Error"
+
+
+
     def safe_to_int(self, num):
-        print(num)
         if int(num) == num:
             return int(num)
         return num
     
-    def mousePressEvent(self, event):
+    def mousePressEvent(self, event) -> None:
         self.oldPos = event.globalPos()
 
-    def mouseMoveEvent(self, event):
+    def mouseMoveEvent(self, event) -> None:
         delta = QPoint (event.globalPos() - self.oldPos)
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = event.globalPos()
